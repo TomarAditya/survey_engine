85538ed63ee0839ff7804a0e4fa8a54ce7a61fd0
aditya <angular.aditya@gmail.com>
2017-08-18 01:31:14 +0530

    removed super_admin

--- src/com/view/AdminLoginPage.java
+++ src/com/view/AdminLoginPage.java
@@ -14,34 +14,32 @@ public class AdminLoginPage {
 		JFrame frame=new JFrame("Admin Login");
 
 		final JTextField textField1=new JTextField();
-		final JTextField textField3 = new JTextField();
+	
 		JPasswordField passwordField1 = new JPasswordField();
-		JPasswordField passwordField2 = new JPasswordField();
+		
 
 		JButton button1=new JButton("Submit");
 		JButton button2=new JButton("Cancel");
-		JButton button3=new JButton("Submit");
-		JButton button4=new JButton("Cancel");
+		
+		JButton signupButton = new JButton("admin signup");
+				
 
 		JLabel label1=new JLabel("AdminId");
 		JLabel label2=new JLabel("Password");
-		JLabel label3=new JLabel("SuperAdminId");
-		JLabel label4=new JLabel("Password");
+	
 
 		label1.setBounds(50,50, 100,30);
 		label2.setBounds(50,100, 100,30);
-		label3.setBounds(350,50, 100,30);
-		label4.setBounds(350,100, 100,30);
+		
 
 		textField1.setBounds(150,50, 100,30);
 		passwordField1.setBounds(150,100, 100,30);
-		textField3.setBounds(550,50, 100,30);
-		passwordField2.setBounds(550,100, 100,30);
+		
 
 		button1.setBounds(50,200,95,30);
 		button2.setBounds(150,200,95,30);
-		button3.setBounds(350,200,95,30);
-	    button4.setBounds(450,200,95,30);
+		
+	    signupButton.setBounds(450,250,200,30);
 
 	    button1.addActionListener(new ActionListener(){
 		    public void actionPerformed(ActionEvent actionEvent){
@@ -67,45 +65,34 @@ public class AdminLoginPage {
 		            frame.dispose();
 		    }  
 	    });
-	    button3.addActionListener(new ActionListener(){  
+	  
+	    
+	    signupButton.addActionListener(new ActionListener(){  
 		    public void actionPerformed(ActionEvent e){  
-		    	
-		    	SuperAdmin superAdmin = new SuperAdmin();
-		    	superAdmin.setUserName(textField3.getText());
-		    	superAdmin.setPassword(passwordField2.getText());
-
-				SuperAdminService superAdminService = new SuperAdminService();
-		    	if(superAdminService.searchSuperAdminService(superAdmin)){
+		   	    
 		    		
-		    		SuperAdminWorkArea superAdminWorkArea = new SuperAdminWorkArea();
-		    		superAdminWorkArea.add();
-		    		frame.dispose();
-		    	}
+		    	AddAdminPage addAdminPage = new AddAdminPage();
+				addAdminPage.add();
+				frame.dispose();		    	
 		    	
 		    }  
 	    });
-	    button4.addActionListener(new ActionListener(){  
-		    public void actionPerformed(ActionEvent e){  
-		    	WelcomePage welcomePage = new WelcomePage();
-	            welcomePage.add();
-	            frame.dispose();
-		    }  
-	    });
+	    
+	   
 
 		frame.add(label1);
 		frame.add(label2);
-		frame.add(label3);
-		frame.add(label4);
+		
 
 		frame.add(textField1);
 		frame.add(passwordField1);
-		frame.add(textField3);
-		frame.add(passwordField2);
+		
+	
 
 		frame.add(button1);
 		frame.add(button2);
-		frame.add(button3);
-		frame.add(button4);
+		
+		frame.add(signupButton);
 
 		frame.setSize(800,600);
 		frame.setLayout(null);
